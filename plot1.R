4c1ccb78309024ec23bb21f35b9e7bb7f3d89cc4
amyb <amy.basque@gmail.com>
2017-05-30 18:59:16 -0500

    Fixing issue with first plot.

--- plot1.R
+++ plot1.R
@@ -1,11 +1,21 @@
-fulldata<-read.table("household_power_consumption.txt", header=TRUE, sep = ";", na.strings="?", stringsAsFactors = FALSE)
+power_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+temp <- tempfile()
+download.file(power_url, temp)
+unzip(temp, "household_power_consumption.txt")
 
+##Read in data to create data frame. 
+fulldata<-read.table("household_power_consumption.txt", header=TRUE, sep = ";", na.strings="?", stringsAsFactors = FALSE)
+unlink(temp)
+
+## Pull out only data for two dates needed. 
 df<- fulldata[fulldata$Date %in% c("1/2/2007","2/2/2007"),] 
 
+## Create new datetime variable. 
 df$DateTime <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
 
+## Create plot.
 hist(df$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
+## Create png of plot. 
 dev.copy(png, file = "plot1.png")
-
 dev.off()
